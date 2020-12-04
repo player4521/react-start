f213230ec28c204aa5dab3f515dc69d9381dbb77
player0 <nanuri@mac.com>
2020-12-04 13:46:15 +0900

    add insert

--- server/server.js
+++ server/server.js
@@ -3,9 +3,35 @@ const app = express();
 
 // 서버와 시퀄라이즈 연동
 const sequelize = require('./models').sequelize;
+const bodyParser = require('body-parser')
+
 sequelize.sync();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// index.js에 정의된 테이블이어야 함
+const {
+  Teacher,
+  Sequelize: { Op }
+} = require('./models');
+sequelize.query('SET NAMES utf8;');
+
+app.post('/add/data', (req, res) => {
+  console.log(req.body)
+
+  Teacher.create({
+      name : req.body.data
+  })
+  .then( result => {
+      res.send(result)
+  })
+  .catch( err => {
+      console.log(err)
+      throw err;
+  })
+})
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
--- src/App.js
+++ src/App.js
@@ -6,34 +6,41 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      host : 'player0',
-      test : '',
+      name : '',
     }
   }
 
-  // componentDidMount() {
-  //   this._getHost();
-  // }
+  _addData = async(e) => {
+    const { name } = this.state;
+    e.preventDefault();
+    
+    const res = await axios('/add/data', {
+      method : 'POST',
+      data : { 'data' : name },
+      headers: new Headers()
+    })
 
-  // _getHost = async() => {
-  //   const res = await axios.get('/api/host');
-  //   this.setState({ host : res.data.host })
-  // }
-  
-  // DB접속 테스트용------------------------------
-  componentDidMount() {
-    this._dbTest();
+    if(res.data) {
+      alert('데이터를 추가했습니다.');
+      return window.location.reload();
+    }
   }
-  _dbTest = async() => {
-    const res = await axios.get('/api/test');
-    console.log(res.data)
+
+  _nameUpdate(e) {
+    this.setState({ name : e.target.value })
   }
-  // DB접속 테스트용------------------------------
 
   render() {
     return(
       <div className='App'>
-        <h3> Welcome to <u> {this.state.host} </u>  </h3>
+        <h3> Welcome to <u> player </u> 0! </h3>
+        <h5> https://github.com/player4521 </h5>
+
+        <br />
+        <form method='POST' onSubmit={this._addData}>
+          <input type='text' maxLength='10' onChange={(e) => this._nameUpdate(e)}/>
+          <input type='submit' value='Add' />
+        </form>
       </div>
     )
   }
